79f8a91af1b7ff7325b667373ba1771319788f43
SifiFox <dima.antonovich.1999@gmail.com>
2023-01-20 09:31:55 +0300

    init: init project; feat: add header & footer components

--- src/pages/main/main-page.jsx
+++ src/pages/main/main-page.jsx
@@ -1,5 +1,7 @@
+import { Header } from '../../components/header-component';
+
 export const MainPage = () => (
-    <section className='main-page'>
-        <div>Main-page</div>
-    </section>
+  <section className='main-page'>
+    <Header />
+  </section>
 );
